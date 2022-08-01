684db03c8a33e128a75996620f129dd785d57141
Jonathan <jhyahav2@gmail.com>
2022-08-01 17:44:40 +0300

    basic box work

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,21 +1,36 @@
-import { ThreeElements, useFrame } from "@react-three/fiber";
-import { MutableRefObject, useRef } from "react";
+import { Float, useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
 import * as THREE from "three";
+import { Vector3 } from "three";
 
-export default function Box() {
+export default function Box({ initialPosition }: { initialPosition: Vector3 }) {
+  const [clicked, setClicked] = useState(false);
   const mesh = useRef<THREE.Mesh>(null);
+  const scroll = useScroll();
   useFrame((state, delta) => {
+    if (clicked && mesh.current) {
+      state.camera.lookAt(mesh.current?.position);
+      state.camera.position.lerp(mesh.current?.position, 0.1);
+      state.camera.updateProjectionMatrix();
+    }
     mesh.current
-      ? (mesh.current.rotation.y = mesh.current.rotation.x += 0.01)
+      ? (mesh.current.rotation.y = mesh.current.rotation.x +=
+          0.1 * scroll.offset) &&
+        (mesh.current.position.z = initialPosition.z + 10 * scroll.offset)
       : null;
   });
 
   return (
-    <>
-      <mesh ref={mesh}>
+    <Float floatingRange={[-1, 1]} floatIntensity={1.5} speed={2}>
+      <mesh
+        ref={mesh}
+        position={initialPosition}
+        onClick={() => setClicked(!clicked)}
+      >
         <boxBufferGeometry args={[1, 1, 1]} />
         <meshPhysicalMaterial />
       </mesh>
-    </>
+    </Float>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,30 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { Canvas } from "@react-three/fiber";
-import { Stars } from "@react-three/drei";
+import { ScrollControls, Stars } from "@react-three/drei";
 import Box from "../components/Box";
+import { Vector3 } from "three";
 
 const Home: NextPage = () => {
   return (
     <main id="canvas_container">
       <Canvas>
+        <ScrollControls distance={10}>
+          <Box initialPosition={new Vector3(1, 1, -10)} />
+          <Box initialPosition={new Vector3(7, 4, -3)} />
+        </ScrollControls>
         <Stars
           radius={100}
-          depth={50}
+          depth={30}
           count={5000}
           factor={4}
           saturation={0}
           fade
-          speed={1}
+          speed={1.25}
         />
 
         <ambientLight intensity={0.1} />
         <directionalLight color={"red"} position={[0, 0, 5]} />
-        <Box />
       </Canvas>
     </main>
   );
