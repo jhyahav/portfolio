21692868e4e30d175ecf958adaeabb78a9853f91
Jonathan <jhyahav2@gmail.com>
2022-08-15 16:04:24 +0300

    added orbit torus next step is to put asteroids on it

--- components/AsteroidBelt.tsx
+++ components/AsteroidBelt.tsx
@@ -1,5 +1,8 @@
-import { useLoader } from "@react-three/fiber";
-import { RepeatWrapping, TextureLoader, Vector3 } from "three";
+import { Torus } from "@react-three/drei";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Mesh, RepeatWrapping, TextureLoader, Vector3 } from "three";
+import { getStarPosition } from "../constants";
 import Asteroid, { AsteroidProps } from "./Asteroid";
 
 const [COUNT, MIN_X, MAX_X, MIN_Y, MAX_Y, ZVAL] = [
@@ -7,6 +10,18 @@ const [COUNT, MIN_X, MAX_X, MIN_Y, MAX_Y, ZVAL] = [
 ];
 
 export default function AsteroidBelt() {
+  const torusRef = useRef<Mesh>(null);
+  useEffect(() => {
+    if (torusRef.current) {
+      torusRef.current.rotation.x = 14.99;
+      torusRef.current.rotation.y = 0.1;
+    }
+  }, [torusRef.current]);
+  useFrame((state, delta) => {
+    if (torusRef.current) {
+      torusRef.current.rotation.z += 0.01;
+    }
+  });
   const colorMaps = useLoader(TextureLoader, ["/rock.webp"]);
   const displacementMaps = useLoader(TextureLoader, [
     "/example_displacement.jpg",
@@ -16,7 +31,6 @@ export default function AsteroidBelt() {
     texture.wrapT = RepeatWrapping;
   });
   const props: Array<AsteroidProps> = [];
-
   for (let i = 0; i < COUNT; i++) {
     const position = new Vector3(
       randInt(MAX_X, MIN_X),
@@ -29,6 +43,7 @@ export default function AsteroidBelt() {
       1 + Math.random() / 2
     );
     const radius = randInt(10, 1);
+    //TODO: randomize colors
     const color = 0x444444;
     //FIXME: make sure taking the length and not (length-1) works fine. it should, since random() never returns 1.
     const displacementMap =
@@ -47,7 +62,18 @@ export default function AsteroidBelt() {
   const asteroids = props.map((propsObj, index) => (
     <Asteroid {...propsObj} key={index} />
   ));
-  return <>{asteroids}</>;
+  return (
+    <group>
+      {asteroids}
+      <Torus
+        ref={torusRef}
+        args={[1100, 100, 10, 32]}
+        position={getStarPosition().lerp(new Vector3(-600, 0, 0), 0.25)}
+      >
+        <meshBasicMaterial visible={true} color={0xff0000} />
+      </Torus>
+    </group>
+  );
 }
 
 // assumes inputs are integers
